f44bf141f68c71fbd12e6ed90016099b16136c5d
somesetty manideep <104755035+somesettymanideep@users.noreply.github.com>
2026-02-18 11:20:10 +0530

    Added in the website

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,8 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
+    base:  "/cricket-ground-cheer/",
+
     host: "::",
     port: 8080,
     hmr: {
